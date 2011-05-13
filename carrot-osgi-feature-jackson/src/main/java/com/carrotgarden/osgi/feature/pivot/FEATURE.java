c13e02cbaf1d82043ab2d3ceaf7811e516e78701
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2011-05-13 12:35:02 -0500

    a

---
+++ carrot-osgi-feature-jackson/src/main/java/com/carrotgarden/osgi/feature/pivot/FEATURE.java
@@ -0,0 +1,56 @@
+package com.carrotgarden.osgi.feature.pivot;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.json.JSONSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.carrotgarden.osgi.feature.FeatureCollection;
+
+public class FEATURE {
+
+	private static final Logger log = LoggerFactory.getLogger(FEATURE.class);
+
+	public static FeatureCollection decode(URL jsonURL) {
+
+		try {
+
+			InputStream input = jsonURL.openStream();
+
+			JSONSerializer codec = new JSONSerializer();
+
+			Map<String, ?> map = (Map<String, ?>) codec.readObject(input);
+
+			log.info("map : {}", map);
+
+			FeatureCollection result = new FeatureCollection();
+
+			return result;
+
+		} catch (Exception e) {
+			log.error("", e);
+		}
+
+		return null;
+
+	}
+
+	public static String encode(FeatureCollection instance) {
+
+		// JSONSerializer codec = new JSONSerializer();
+		// codec.setAlwaysDelimitMapKeys(true);
+
+		try {
+			return JSONSerializer.toString(instance, true);
+		} catch (Exception e) {
+			log.error("", e);
+		}
+
+		return null;
+
+	}
+
+}
