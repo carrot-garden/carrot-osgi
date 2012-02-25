bf317f482a19abfc0f3d574ccf66a12028da304f
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-02-24 20:12:00 -0600

    a

---
+++ carrot-osgi-anno-scr/carrot-osgi-anno-scr-make/src/test/java/com/carrotgarden/osgi/anno/scr/make/TestMaker2.java
@@ -0,0 +1,85 @@
+package com.carrotgarden.osgi.anno.scr.make;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMaker2 {
+
+	static final Logger log = LoggerFactory.getLogger(TestMaker2.class);
+
+	static String convertStreamToString(final InputStream input) {
+
+		if (input == null) {
+			return null;
+		}
+
+		try {
+			return new java.util.Scanner(input).useDelimiter("\\A").next();
+		} catch (final Exception e) {
+			return "";
+		}
+	}
+
+	static void testClass(final String name) throws Throwable {
+
+		final ClassLoader loader = TestMaker2.class.getClassLoader();
+
+		log.debug("######################################");
+		log.debug("test class : {}", name);
+
+		final Maker maker = new Maker();
+
+		final String source = maker.make(loader, name);
+		log.debug("source : \n{}", source);
+
+		final Class<?> klaz = Class.forName(name);
+
+		final String fileName = klaz.getSimpleName() + ".xml";
+		final InputStream input = klaz.getResourceAsStream(fileName);
+		final String target = convertStreamToString(input);
+		log.debug("target : \n{}", target);
+
+		assertEquals(source, target);
+
+	}
+
+	@Test
+	public void test01() throws Throwable {
+
+		final String pack = "com.carrotgarden.osgi.anno.scr.case01.";
+
+		testClass(pack + "Comp_01_empty");
+
+		testClass(pack + "Comp_01_with_name");
+
+		testClass(pack + "Comp_01_service");
+
+		testClass(pack + "Comp_01_reference");
+
+		testClass(pack + "Comp_01_reference_dynamic");
+
+		testClass(pack + "Comp_01_factory");
+
+	}
+
+	@Test
+	public void test02() throws Throwable {
+
+		final String pack = "com.carrotgarden.osgi.anno.scr.case02.";
+
+		testClass(pack + "Comp_02_0"); // missing
+
+		testClass(pack + "Comp_02_1");
+
+		testClass(pack + "Comp_02_2");
+
+		testClass(pack + "Comp_02_3"); // missing
+
+	}
+
+}
